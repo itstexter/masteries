b4a7b2c19a7a6b19350e87899286979f6c05f5c8
Alex Texter <attexter@amazon.com>
2014-09-26 16:07:15 -0700

    First commit

---
+++ scripts/boxfiller.js
@@ -0,0 +1,216 @@
+availablePoints = 30;
+
+var Tree = function(value) {
+	this.points = 0;
+	this.value = value;
+
+	this.updateTree = function(isIncrementing) {
+		var i;
+		var startingMasteryId = this.value * 24;
+		for (i = startingMasteryId; i < startingMasteryId + 23; i++) {
+			var id = "m" + i;
+			var currentMastery = masteries[id];
+			blah = currentMastery;
+			var rowReqsMet = currentMastery.row * 4 <= this.points;
+
+			if (isIncrementing && currentMastery.isActive()) {
+					$("#" + id).find(".colorBlock").show();
+					$("#" + id).find(".greyBlock").hide();
+			} else {
+				$("#" + id).find(".colorBlock").hide();
+				$("#" + id).find(".greyBlock").show();				
+			}
+		}
+	}
+};
+
+var treeNames = {
+	OFFENSE: 0,
+	DEFENSE: 1,
+	UTILITY: 2
+};
+
+var offenseTree = new Tree(treeNames.OFFENSE);
+var defenseTree = new Tree(treeNames.DEFENSE);
+var utilityTree = new Tree(treeNames.UTILITY);
+
+var Mastery = function(data, tree, row, coordinates) {
+	this.id = data["id"];
+	this.current = 0;
+	this.maximum = data["max"];
+	this.tree = tree;
+	this.row = row;
+	this.coordinates = coordinates;
+	this.description = data["description"];
+	this.dependency = data["dependency"];
+	this.title = data["title"];
+
+	this.isActive = function() {
+		var rowReqsMet = (this.row * 4 <= this.tree.points) && this.current < this.maximum;
+		var dependencyMet = true;
+		if (typeof(this.dependency) != "undefined") {
+			var dependencyMastery = masteries[this.dependency];
+			dependencyMet = dependencyMastery.current == dependencyMastery.maximum;
+		}
+		return (rowReqsMet && dependencyMet);
+	};
+
+	this.canDecrement = function() {
+		if (!this.isActive()) {
+			return false;
+		}
+		var start = parseInt(this.id.substring(1));
+		var i;
+		for (i = start + 1; i < (this.tree.value + 1) * 24 - 1; i++) {
+			var m = masteries["m" + i];
+			if (m.isActive()) {
+				this.tree.points -= 1;
+				if (!m.isActive()) {
+					this.tree.points += 1;
+					return false;
+				}
+				this.tree.points += 1;
+			}
+		}
+		return true;
+	};
+
+	this.increment = function(div) {
+		if (!this.isActive()) {
+			return;
+		} 
+		this.current += 1;
+		this.tree.points += 1;
+		availablePoints -= 1;
+		div.find(".pointBlock").text(this.calcPoints());
+		this.tree.updateTree(true);
+	}
+
+	this.decrement = function(div) {
+		if (!this.canDecrement()) {
+			return;
+		}
+		console.log("decrement");
+		this.current -= 1;
+		this.tree.points -= 1;
+		availablePoints += 1;
+		div.find(".pointBlock").text(this.calcPoints());
+		this.tree.updateTree(false);
+	}
+
+	this.calcPoints = function() {
+		return this.current + "/" + this.maximum;
+	}
+};
+
+var getTree = function(treeNum) {
+	switch(treeNum) {
+		case 0: 
+			return offenseTree;
+		case 1: 
+			return defenseTree;
+		case 2: 
+			return utilityTree;
+	}
+};
+
+masteries = {};
+
+$(document).ready(function() {
+
+	fillData();
+	buildDom();
+	updateCounters();
+
+});
+
+var fillData = function() {
+	var mNum = 0;
+	var xCount = 0;
+	var yCount = 0;
+	var treeNum = 0;
+	while (yCount < 6) {
+		while (xCount < 10) {
+
+			if (mNum > 71) {
+				break;
+			}
+			if (mNum != 0 && mNum % 24 == 0) {
+				treeNum += 1;
+			}
+
+			var id = "m" + mNum;
+			var currentRow = parseInt((mNum - (treeNum * 24)) / 4);
+			var masteryData = masteriesData[mNum];
+
+			masteryData["tree"] = treeNum;
+			masteryData["row"] = currentRow;
+
+			var x = 480 - xCount * 48;
+			var y = 288 - yCount * 48;
+			coordinates = "" + x + "px " + y + "px";
+			masteryData["coordinates"] = coordinates;
+
+			var mastery = new Mastery(masteryData, getTree(treeNum), currentRow, coordinates);
+			masteries[id] = mastery;
+			mNum++;
+
+			if (masteryData["invis"]) {
+				continue;
+			};
+			xCount++;
+		}
+		xCount = 0;
+		yCount++;
+	}
+}
+
+var buildDom = function() {
+	var i = 0;
+	for (i = 0; i < 72; i++) {
+		var id = "m" + i;
+		var mastery = masteries[id];
+
+		var box = $("#" + id);
+
+		var $colorBlock = $("<div />").addClass("colorBlock");
+		box.append($colorBlock);
+		$colorBlock.css("background-position", mastery.coordinates);
+
+		var $greyBlock = $("<div />").addClass("greyBlock");
+		box.append($greyBlock);
+		$greyBlock.css("background-position", mastery.coordinates);
+
+		var $pointBlock = $("<div />").addClass("pointBlock").text(mastery.calcPoints());
+		box.append($pointBlock);
+
+		var $divider = $("<div />").addClass("divider");
+		box.append($divider);
+
+		box.mousedown(function(event) {
+			var $clickedMastery = $(event.currentTarget);
+			var currentMastery = masteries[$clickedMastery.attr("id")];
+
+			switch(event.which) {
+				case 1:
+					currentMastery.increment($clickedMastery);
+					break;
+				case 2:
+					currentMastery.decrement($clickedMastery);
+					break;
+			}
+			updateCounters();
+		});
+	};
+
+	offenseTree.updateTree(true);
+	defenseTree.updateTree(true);
+	utilityTree.updateTree(true);
+}
+
+var updateCounters = function() {
+	$("#availablePoints").text(availablePoints);
+	$("#offensePoints").text(offenseTree.points);
+	$("#defensePoints").text(defenseTree.points);
+	$("#utilityPoints").text(utilityTree.points);
+}
